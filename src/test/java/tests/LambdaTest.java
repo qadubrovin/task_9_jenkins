2a0a402d0a972b06b4da319fc5a9685b20613c57
qadubrovin <qadubrovin@yandex.ru>
2021-06-17 20:35:36 +0300

    added test with configuration for jenkins and selenoid

---
+++ src/test/java/tests/LambdaTest.java
@@ -0,0 +1,40 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byAttribute;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class LambdaTest extends TestBase {
+
+    private static final String BASE_URL = "https://github.com";
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE_NUMBER = 68;
+
+    @Test
+    public void issueSearchLambdaTest() {
+        step("Открываем главную страницу", (s) -> {
+            s.parameter("URL", BASE_URL);
+            open(BASE_URL);
+        });
+        step("Ищем репозиторий", (s) -> {
+            s.parameter("repository", REPOSITORY);
+            $(".header-search-input").setValue(REPOSITORY).submit();
+        });
+        step("Переходим в репозиторий", (s) -> {
+            s.parameter("repository", REPOSITORY);
+            $(byAttribute("href", "/" + REPOSITORY)).click();
+        });
+        step("Открываем таб Issues в репозитории", () -> {
+            $(withText("Issues")).click();
+        });
+        step("Проверяем, что Issue с номером " + ISSUE_NUMBER + " существет", (s) -> {
+            s.parameter("Number", ISSUE_NUMBER);
+            $(withText("#" + ISSUE_NUMBER)).should(visible);
+        });
+    }
+}
