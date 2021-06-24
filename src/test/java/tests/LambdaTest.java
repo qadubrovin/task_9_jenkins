45ea899272aebedd8a03d163e776b92e01361bb9
qadubrovin <qadubrovin@yandex.ru>
2021-06-24 17:04:13 +0300

    adding fake tests

--- src/test/java/tests/LambdaTest.java
+++ src/test/java/tests/LambdaTest.java
@@ -15,6 +15,7 @@ public class LambdaTest extends TestBase {
     private static final String BASE_URL = "https://github.com";
     private static final String REPOSITORY = "eroshenkoam/allure-example";
     private static final int ISSUE_NUMBER = 68;
+    private static final int ISSUE_NUMBER4 = 168;
 
     @Test
     @Tag("terminal")
@@ -37,6 +38,78 @@ public class LambdaTest extends TestBase {
         step("Проверяем, что Issue с номером " + ISSUE_NUMBER + " существет", (s) -> {
             s.parameter("Number", ISSUE_NUMBER);
             $(withText("#" + ISSUE_NUMBER)).should(visible);
+        });
+    }
+
+    @Test
+    @Tag("terminal")
+    public void issueSearchLambdaTest2() {
+        step("Открываем главную страницу", (s) -> {
+            s.parameter("URL", BASE_URL);
+            open(BASE_URL);
+        });
+        step("Ищем репозиторий", (s) -> {
+            s.parameter("repository", REPOSITORY);
+            $(".header-search-input").setValue(REPOSITORY).submit();
+        });
+        step("Переходим в репозиторий", (s) -> {
+            s.parameter("repository", REPOSITORY);
+            $(byAttribute("href", "/" + REPOSITORY)).click();
+        });
+        step("Открываем таб Issues в репозитории", () -> {
+            $(withText("Issues")).click();
+        });
+        step("Проверяем, что Issue с номером " + ISSUE_NUMBER + " существет", (s) -> {
+            s.parameter("Number", ISSUE_NUMBER);
+            $(withText("#" + ISSUE_NUMBER)).should(visible);
+        });
+    }
+
+    @Test
+    @Tag("terminal")
+    public void issueSearchLambdaTest3() {
+        step("Открываем главную страницу", (s) -> {
+            s.parameter("URL", BASE_URL);
+            open(BASE_URL);
+        });
+        step("Ищем репозиторий", (s) -> {
+            s.parameter("repository", REPOSITORY);
+            $(".header-search-input").setValue(REPOSITORY).submit();
+        });
+        step("Переходим в репозиторий", (s) -> {
+            s.parameter("repository", REPOSITORY);
+            $(byAttribute("href", "/" + REPOSITORY)).click();
+        });
+        step("Открываем таб Issues в репозитории", () -> {
+            $(withText("Issues")).click();
+        });
+        step("Проверяем, что Issue с номером " + ISSUE_NUMBER + " существет", (s) -> {
+            s.parameter("Number", ISSUE_NUMBER);
+            $(withText("#" + ISSUE_NUMBER)).should(visible);
+        });
+    }
+
+    @Test
+    @Tag("terminal")
+    public void issueSearchLambdaTest4() {
+        step("Открываем главную страницу", (s) -> {
+            s.parameter("URL", BASE_URL);
+            open(BASE_URL);
+        });
+        step("Ищем репозиторий", (s) -> {
+            s.parameter("repository", REPOSITORY);
+            $(".header-search-input").setValue(REPOSITORY).submit();
+        });
+        step("Переходим в репозиторий", (s) -> {
+            s.parameter("repository", REPOSITORY);
+            $(byAttribute("href", "/" + REPOSITORY)).click();
+        });
+        step("Открываем таб Issues в репозитории", () -> {
+            $(withText("Issues")).click();
+        });
+        step("Проверяем, что Issue с номером " + ISSUE_NUMBER + " существет", (s) -> {
+            s.parameter("Number", ISSUE_NUMBER4);
+            $(withText("#" + ISSUE_NUMBER4)).should(visible);
         });
     }
 }
